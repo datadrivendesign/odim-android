f14b8ad0c4b456e6270b8a10b830a9dbad26b976
zhangmichael1 <zhilinz2@illinois.edu>
2020-03-14 20:25:38 -0500

    Improved naming

--- ui_recording/app/src/androidTest/java/edu/illinois/recordingservice/ExampleInstrumentedTest.java
+++ ui_recording/app/src/androidTest/java/edu/illinois/recordingservice/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("edu.illinois.finalproject", appContext.getPackageName());
+        assertEquals("edu.illinois.recordingservice", appContext.getPackageName());
     }
 }
