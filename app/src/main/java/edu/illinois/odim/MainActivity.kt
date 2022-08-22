14ae723961e6ce19052ca691e753f4166edf78ce
Carl Guo <carlguo2@illinois.edu>
2022-08-22 14:50:26 -0500

    Clean up imports

--- app/src/main/java/edu/illinois/odim/MainActivity.kt
+++ app/src/main/java/edu/illinois/odim/MainActivity.kt
@@ -1,7 +1,6 @@
 package edu.illinois.odim
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
