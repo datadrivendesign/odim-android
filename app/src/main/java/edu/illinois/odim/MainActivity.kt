f2bb5eba14bb1c7fef7961607e56a419b8b6d642
Carl Guo <carlguo2@illinois.edu>
2022-08-22 12:18:46 -0500

    Allow input to set user id
    MainActivity now prompts for a user id which is used to set amplify S3 storage location for traces.

--- app/src/main/java/edu/illinois/odim/MainActivity.kt
+++ app/src/main/java/edu/illinois/odim/MainActivity.kt
@@ -1,17 +1,23 @@
 package edu.illinois.odim
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 // this should be static
 private var recyclerAdapter : CustomAdapter? = null
+internal var userId = "test_user15"
+
 fun notifyPackageAdapter() {
     recyclerAdapter?.notifyDataSetChanged()
 }
@@ -24,6 +30,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // found code from: https://handyopinion.com/show-alert-dialog-with-an-input-field-edittext-in-android-kotlin/
+        // set up user id
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Input user id")
+
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+
+        builder.setPositiveButton("DONE", { dialogInterface, which ->
+            userId = input.text.toString()
+            Log.i("userId", userId)
+        })
+        builder.setNegativeButton("CANCEL", { dialogInterface, which ->
+            dialogInterface.cancel()
+        })
+        builder.show()
+        // set up package view
         title = " Packages"
 
         recyclerView = findViewById(R.id.packageRecyclerView)
@@ -47,7 +71,5 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
-
-
 }
 
