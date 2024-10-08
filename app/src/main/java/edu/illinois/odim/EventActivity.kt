07c2863b7855ab7827d07d2a90447d24c3d02d96
Carl Guo <carlguo2@illinois.edu>
2024-10-08 10:51:19 -0500

    Start on implementing multi-select menu

--- app/src/main/java/edu/illinois/odim/EventActivity.kt
+++ app/src/main/java/edu/illinois/odim/EventActivity.kt
@@ -8,6 +8,10 @@ import android.graphics.Paint
 import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -96,6 +100,47 @@ class EventActivity : AppCompatActivity() {
         }
         uploadTraceButton?.isEnabled = isTraceComplete
     }
+
+    private val multiSelectActionModeCallback = object : ActionMode.Callback {
+        // Called when the action mode is created. startActionMode() is called.
+        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+            // Inflate a menu resource providing context menu items.
+            val inflater: MenuInflater = mode.menuInflater
+            inflater.inflate(R.menu.multi_select_menu, menu)
+            return true
+        }
+
+        // Called each time the action mode is shown. Always called after
+        // onCreateActionMode, and might be called multiple times if the mode
+        // is invalidated.
+        override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+            return false // Return false if nothing is done
+        }
+
+        // Called when the user selects a contextual menu item.
+        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.menu_split_trace -> {
+                    Log.i("EVENT", "SPLIT")
+                    mode.finish() // Action picked, so close the CAB.
+                    true
+                }
+                R.id.menu_delete_screens -> {
+                    Log.i("EVENT", "DELETE")
+                    mode.finish()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Called when the user exits the action mode.
+        override fun onDestroyActionMode(mode: ActionMode) {
+            recyclerAdapter?.isMultiSelectMode = false
+//            actionMode = null
+        }
+    }
+
 
     override fun onStop() {
         super.onStop()
